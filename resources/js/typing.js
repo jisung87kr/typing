834708ed6ec441ba3d0a3dcbae41168f60731f7d
jisungyoo <jisung87kr@gmail.com>
2023-02-20 22:37:12 +0900

    breeze 설치

--- resources/js/typing.js
+++ resources/js/typing.js
@@ -2,17 +2,13 @@ import Jquery from 'jquery'
 import 'bootstrap';
 import './bootstrap';
 import Swal from 'sweetalert2'
-import Alpine from "alpinejs";
 import { createApp } from 'vue'
 import App from './vue/App.vue'
-// import '../sass/app.scss'
+import '../sass/app.scss'
 import 'animate.css';
 
 window.$ = Jquery;
 window.Swal = Swal;
-window.Alpine = Alpine;
-
-Alpine.start();
 
 const app = createApp(App)
 app.mount('#app')
